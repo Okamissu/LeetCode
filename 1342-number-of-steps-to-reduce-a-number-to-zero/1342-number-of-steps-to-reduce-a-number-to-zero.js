5639b1b19d60472c0dee7f7f356e69356a589d12
Kamil Kobylarz <okamissu@gmail.com>
2026-07-05 14:32:00 +0200

    Time: 0 ms (100%), Space: 53.6 MB (62.2%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -14,3 +14,15 @@ const numberOfSteps = function (num) {
     return steps
 };
 
+// Base solution
+
+// const numberOfSteps = function(num) {
+//     let steps = 0
+
+//     while(num !== 0){
+//         num = num % 2 === 0 ? num / 2 : num - 1
+//         steps++
+//     }
+
+//     return steps
+// };
