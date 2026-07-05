ea1bec57362fab707650d696bfd6a774f99c5da5
Kamil Kobylarz <okamissu@gmail.com>
2026-07-05 13:24:01 +0200

    Time: 2 ms (23.03%), Space: 55.3 MB (98.58%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -3,14 +3,12 @@
  * @return {string[]}
  */
 const fizzBuzz = function(n) {
-    const numArray = Array.from({ length: n}, (_, i) => i+1  )
+    return Array.from({ length: n }, (_, i) => {
+        const num = i + 1;
 
-    const answer = numArray.map(num => {
-    if (num % 15 === 0) return "FizzBuzz"
-    if (num % 5 === 0) return "Buzz"
-    if (num % 3 === 0) return "Fizz"
-    return "" + num
-    })
-
-    return answer
+        if (num % 15 === 0) return "FizzBuzz";
+        if (num % 3 === 0) return "Fizz";
+        if (num % 5 === 0) return "Buzz";
+        return String(num);
+    });
 };
