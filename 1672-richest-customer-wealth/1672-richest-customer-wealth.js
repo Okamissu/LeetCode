961b1da942984c0eea66ba0ad3f6bf6f952c0c5d
Kamil Kobylarz <okamissu@gmail.com>
2026-07-05 13:08:30 +0200

    Time: 0 ms (100%), Space: 54.2 MB (30.92%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.js
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -2,16 +2,6 @@
  * @param {number[][]} accounts
  * @return {number}
  */
-const maximumWealth = function(accounts) {
-    let maxWealth = 0
-
-    for(let client of accounts){
-        let clientWealth = 0
-        for(let bank of client){
-            clientWealth += bank 
-        }
-    if(clientWealth > maxWealth) maxWealth = clientWealth
-    }
-
-    return maxWealth
+const maximumWealth = function (accounts) {
+    return accounts.reduce((max, client) => Math.max(max, client.reduce((bank, acc) => acc + bank, 0)), 0)
 }
