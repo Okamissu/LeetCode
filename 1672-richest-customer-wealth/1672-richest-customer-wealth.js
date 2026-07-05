77fc42de431c4ca5af73ae35e380ec3f71504d15
Kamil Kobylarz <okamissu@gmail.com>
2026-07-05 13:09:08 +0200

    Time: 0 ms (100%), Space: 54.3 MB (30.92%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.js
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -3,5 +3,9 @@
  * @return {number}
  */
 const maximumWealth = function (accounts) {
-    return accounts.reduce((max, client) => Math.max(max, client.reduce((bank, acc) => acc + bank, 0)), 0)
-}
+    return accounts.reduce(
+        (max, client) =>
+            Math.max(max, client.reduce((sum, bank) => sum + bank, 0)),
+        0
+    );
+};
